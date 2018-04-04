c12df5f3bf95bf57cce182494adccac99abc28a2
Meghan Debity <mdebity@gmail.com>
2018-04-04 11:49:37 -0500

    added comments to js file

---
+++ main.js
@@ -0,0 +1,4 @@
+// Write a program that prints the numbers from 1 to 100. 
+// For multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number
+// For the multiples of seven (7, 14, 21, etc.) print "Monkey". 
+// For numbers which are multiples of both five and seven print "ChickenMonkey".
