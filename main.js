a4633833cc9f26813877ebd2e44a9dc37d475879
Meghan Debity <mdebity@gmail.com>
2018-04-04 13:50:45 -0500

    on right track with array; including new notes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // Write a program that prints the numbers from 1 to 100. 
+const number = i
+
+for (i = 0; i < number.length; i++) {
+
+}
 // For multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number
 // For the multiples of seven (7, 14, 21, etc.) print "Monkey". 
 // For numbers which are multiples of both five and seven print "ChickenMonkey".
